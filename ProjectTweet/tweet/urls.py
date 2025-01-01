529532b6b86ec77ce51f622cef8811e0c1822a9b
santoshs0612 <santoshs0612@gmail.com>
2025-01-01 18:21:01 +0530

    Initial configurations and app setups done

---
+++ ProjectTweet/tweet/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name= "index"),
+]
