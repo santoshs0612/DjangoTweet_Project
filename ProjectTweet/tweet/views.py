a4bb216cd6ec94356285d7747c5587d7273234d0
santoshs0612 <santoshs0612@gmail.com>
2025-01-01 20:05:57 +0530

    view compleated

--- ProjectTweet/tweet/views.py
+++ ProjectTweet/tweet/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
-
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+def tweetList(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request,"tweetsList.html",{"tweets":tweets})
+
+def tweetCreate(request):
+    if request.method=="POST":
+        form = TweetForm(request.POST,request.FILES)
+        if form.is_vald():
+            tweet =form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect("tweetList")
+    else:
+        form = TweetForm()
+    return render(request,'tweetForm.html',{"form":form})
+
+def tweetEdit(request,tweetId):
+    tweet = get_object_or_404(Tweet,pk=tweetId,user = request.user)
+    if request.method == "POST":
+        form = TweetForm(request.POST,request.FILES,instance=tweet)
+        if form.is_vald():
+            tweet =form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect("tweetList")
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request,'tweetForm.html',{"form":form})
+def tweetDelete(request,tweetId):
+    tweet = get_object_or_404(Tweet,pk=tweetId,user = request.user)
+    
+    if request.method =="POST":
+        tweet.delete()
+        return redirect("tweetList")
+    return render(request,'tweetDelete.html',{"tweet":tweet})
